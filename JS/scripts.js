076b233d17ba794d7b796f42e587487b94e673b1
asmohr714 <152821567+asmohr714@users.noreply.github.com>
2024-01-09 01:38:52 -0500

    Initial commit

---
+++ JS/scripts.js
@@ -0,0 +1,42 @@
+let pokemonList = [
+
+    {
+        name: 'Koffing',
+        type: 'Poison', 
+        height: '0.6m',
+        weight: '1kg',
+        abilities: 'Levitate', 
+        hp: 40
+    },
+    {
+        name: 'Physduck',
+        type: 'Water',
+        height: '0.8m',
+        weight: '19.6kg',
+        abilities: ['Damp','Cloud-nine','Swift-swim'],
+        hp: 50
+    },
+    {
+        name: 'Abra',
+        type: 'Psychic', 
+        height: '0.9m',
+        weight: '19.5kg',
+        abilities: ['Synchronize', 'Inner-focus', 'Magic-guard'],
+        hp: 25
+    },
+    {
+        name: 'Caterpie',
+        type: 'Bug',
+        height: '0.3m',
+        weight: '2.9kg',
+        abilities: ['Shield-dust', 'Run-away'],
+        hp: 45,
+    },
+    {
+        name: 'Pidgey',
+        type: ['flying','normal'],
+        height: '0.3m',
+        weight: '1.8kg',
+        abilities: ['Keen-eye','Tangled-feet'],
+        hp: 40,
+    }]
---
+++ README.md
@@ -0,0 +1,11 @@
+# Objective:
+
+To build a small web application with HTML, CSS, and JavaScript that loads
+data from an external API and enables the viewing of data points in detail.
+
+### Exersize 1.1-1.2:
+
+● Learn coding practices focusing on the core principles of JavaScript: objects, arrays,
+conditionals, and loops.
+
+● Set up a general dataset for your application and display it in a very basic way to the user.
