2015e54f91b6508e5c4bec44518090bed6fb39ff
asmohr714 <152821567+asmohr714@users.noreply.github.com>
2024-01-13 23:45:02 -0500

    changed 'for' array to 'foreach'
    changed scritps.js
    changed styles.css

--- JS/scripts.js
+++ JS/scripts.js
@@ -13,7 +13,7 @@ let pokemonList = [
         type: 'Water',
         height: 0.8,
         weight: 19.6,
-        abilities: ['Damp','Cloud-nine','Swift-swim'],
+        abilities: ['Damp',' Cloud-nine',' Swift-swim'],
         hp: 50
     },
     {
@@ -21,7 +21,7 @@ let pokemonList = [
         type: 'Psychic', 
         height: 0.9,
         weight: 19.5,
-        abilities: ['Synchronize', 'Inner-focus', 'Magic-guard'],
+        abilities: ['Synchronize', ' Inner-focus', ' Magic-guard'],
         hp: 25
     },
     {
@@ -29,26 +29,22 @@ let pokemonList = [
         type: 'Bug',
         height: 0.3,
         weight: 2.9,
-        abilities: ['Shield-dust', 'Run-away'],
+        abilities: ['Shield-dust', ' Run-away'],
         hp: 45,
     },
     {
         name: 'Pidgey',
-        type: ['Flying','Normal'],
+        type: ['Flying',' Normal'],
         height: .03,
         weight: 1.8,
-        abilities: ['Keen-eye','Tangled-feet'],
+        abilities: ['Keen-eye', ' Tangled-feet'],
         hp: 40,
     }]
 
-
-    for (let i=0; i<pokemonList.length; i++) {
-        document.write(pokemonList[i].name + ' (height: ' + pokemonList[i].height + ') ');
-        if (pokemonList[i].height <= 0.5) {
+    pokemonList.forEach(function(item) {
+        document.write('Name: ' + item.name + '<br>' + 'Type: ' + item.type + '<br>' + 'Height: ' + item.height + '<br>' + 'Weight: ' + item.weight + '<br>' + 'Abilities: ' + item.abilities + '<br>' + 'HP: ' + item.hp);{
+        document.write('<br>');
         document.write('<br>');
         }
-        else{
-            document.write('That\'s a big Pokemon!');
-            document.write('<br>');
-        }
-        }
+      });
+
