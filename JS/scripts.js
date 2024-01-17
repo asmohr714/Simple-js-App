b9f63048b4ab366833a2ede5537785e571965c42
asmohr714 <152821567+asmohr714@users.noreply.github.com>
2024-01-17 16:58:39 -0500

    added buttons and button event log to pokemon
    changed index.html
    changed scripts.js
    changed sytles.css

--- JS/scripts.js
+++ JS/scripts.js
@@ -1,3 +1,6 @@
+
+
+
 let pokemonRepository = (function () {
 
 let pokemonList = [
@@ -43,31 +46,55 @@ let pokemonList = [
         hp: 40,
     }]
 
-    function add(pokemon) {
-        if (typeof pokemon === 'object' &&
-        'name' in pokemon) {
-            pokemonList.push(pokemon);
-        } else {
-            console.log('Pokemon is not correct')
-        }
-      }
+// Add Pokemon to List
 
-    function getAll() {
-        return pokemonList;
-      }
-    
-    return {
-        add: add,
-        getAll: getAll
-      };
-    })();
+function add(pokemon) {
+    if (typeof pokemon === 'object' &&
+    'name' in pokemon) {
+        pokemonList.push(pokemon);
+    } else {
+        console.log('Pokemon is not correct')
+    }
+}
 
-console.log(pokemonRepository.getAll())
-console.log(pokemonRepository.add({name: 'Koffing'}));
+// Return the Array of Pokemon
+
+function getAll() {
+    return pokemonList;
+}
+
+// Function to Create Pokemon buttons
+
+function addListItem(pokemon){
+    let pokemonList = document.querySelector('.pokemon-list');
+    let listpokemon = document.createElement('li');
+    let button = document.createElement('button');
+    button.innerText = pokemon.name;
+    button.classList.add('button-class');
+    button.addEventListener('click', showDetails)
+    listpokemon.appendChild(button);
+    pokemonList.appendChild(listpokemon);
+}
+
+function showDetails(e) {
+    if (e.target.matches('button')) {
+      console.log(e.target.innerText);
+    }
+}
+
+return {
+    add: add,
+    getAll: getAll,
+    addListItem: addListItem
+    };
+
+})();
+
+// Print List of Pokemon
+
+pokemonRepository.getAll().forEach(function (pokemon) {
+    pokemonRepository.addListItem(pokemon);
+
+});
 
 
-pokemonRepository.getAll().forEach(function (item) {
-    document.write('Name: ' + item.name + '<br>' + 'Type: ' + item.type + '<br>' + 'Height: ' + item.height + '<br>' + 'Weight: ' + item.weight + '<br>' + 'Abilities: ' + item.abilities + '<br>' + 'HP: ' + item.hp);{
-        document.write('<br>');
-        document.write('<br>');
-    }})
